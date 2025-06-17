d07539a7efe37ff49f2ff23990843faf4088a834
tmanikanta12 <245122749305@mvsrec.edu.in>
2025-06-17 15:26:25 +0530

    Add files via upload

---
+++ arithmetic.java
@@ -0,0 +1,12 @@
+class arithmetic
+{
+public static void main(String args[])
+{
+  int a=10;
+  int b=5;
+  System.out.println("add"+(a+b));
+  System.out.println("sub"+(a-b));
+  System.out.println("mul"+(a*b));
+  System.out.println("div"+(a/b));
+  }
+ }
